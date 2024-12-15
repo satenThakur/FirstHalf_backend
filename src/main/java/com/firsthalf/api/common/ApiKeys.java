450af8b1582675e9f50e9a1586218f6c529c18f0
satendThakur <satendra@adroinfotech.in>
2024-12-15 18:10:21 +0530

    new changes

--- src/main/java/com/firsthalf/api/common/ApiKeys.java
+++ src/main/java/com/firsthalf/api/common/ApiKeys.java
@@ -3,11 +3,11 @@ package com.firsthalf.api.common;
 public interface ApiKeys {
     public String RESPONSEDATA ="responseData";
     public String MESSAGE ="message";
-    public final String ACCOUNT_SID="AC4c7520b3acec5be3818fef230783c4a9";
-    public final String AUTH_TOKEN="4ff2209a78eaeddda4637ba1a5c0453e";
-    public final String FROM_NUMBER="+14159660388";
+    public final String ACCOUNT_SID= "AC7f3784cb872f6e183afc4bf6952f6946";//  "AC4c7520b3acec5be3818fef230783c4a9";
+    public final String AUTH_TOKEN="256127470c3d1771afae01d6ff210918";//  "4ff2209a78eaeddda4637ba1a5c0453e";
+    public final String FROM_NUMBER="+14403067680";//"+14159660388";
 
-    public static final Integer EXPIRE_MINS = 2;
+    public static final Integer EXPIRE_MINS = 3;
 
 
 
